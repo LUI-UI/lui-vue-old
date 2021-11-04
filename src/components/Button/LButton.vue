c6bf8211908e431d7053e72e92dc2674b38ffbf0
rhmkstk <rahmi.kstk@gmail.com>
2021-11-04 16:41:55 +0300

    refactor button

--- src/components/Button/LButton.vue
+++ src/components/Button/LButton.vue
@@ -62,49 +62,6 @@ export default {
   },
   emits: ['click'],
   setup(props) {
-    function sizes(size, type) {
-      // sm: x:12 y:6
-      // md: x:16 y:8
-      // lg: x:24 y:12
-      // p-1.5 = 6px
-      // p-2 = 8px
-      // p-3 = 12px
-      // p-4 = 16px
-      // p-6 = 24px
-      if (type === 'link') return 'p-0'
-      if (size === 'sm') return 'px-3 py-1.5'
-      if (size === 'md') return 'px-4 py-2'
-      if (size === 'lg') return 'px-6 py-3'
-    }
-    function block(is, type) {
-      if (!is) return ''
-      if (type === 'link') return ''
-      return 'w-full'
-    }
-    function rounded(is, size, type) {
-      // sm: 6px = rounded-sm
-      // md: 8px = rounded-md
-      // lg: 10px = rounded-lg
-      if (!is) return ''
-      if (type === 'link' || type === 'text') return ''
-      return `rounded-${size}`
-    }
-    function roundedFull(is, type) {
-      if (!is) return ''
-      if (type === 'link' || type === 'text') return ''
-      return 'rounded-full'
-    }
-    // function filter(filter, variant) {
-    //   if (filter === 'darker') return `bg-${variant}-900`
-    //   if (filter === 'lighter') return `bg-${variant}-50`
-    // }
-    function font(size) {
-      // sm: fw: 500, size: 12px = text-xs, leading-4.5: 18px
-      // md: fw: 500, size: 16px, lh: 24px
-      // lg: fw: 500, size: 16px, leading-6: 24px
-      if (size === 'sm') return 'text-sm leading-4.5' // line heigtler kontrol edilecek
-      if (size === 'md' || size === 'lg') return 'text-base leading-6'
-    }
     function types(type, variant) {
       if (type === 'default') return `text-white bg-${variant}`
       if (type === 'outline')
@@ -150,30 +107,22 @@ export default {
       }
       return Object.values(classes).join(' ').toString()
     }
+    // function filter(filter, variant) {
+    //   if (filter === 'darker') return `bg-${variant}-900`
+    //   if (filter === 'lighter') return `bg-${variant}-50`
+    // }
     const classes = computed(() => {
-      return [
-        `${states(props.type, props.variant)}
-         ${sizes(props.size, props.type)}
-         ${types(props.type, props.variant)} 
-         ${font(props.size)} 
-         ${block(props.block, props.type)} 
-         ${rounded(props.rounded, props.size, props.type)} 
-         ${roundedFull(props.roundedFull, props.type)}
-          
-        `,
-      ]
+      let styles = {
+        type: types(props.type, props.variant),
+        state: states(props.type, props.variant),
+        size: props.type === 'link' ? 'p-0' : props.size === 'sm' ? 'px-3 py-1.5' : props.size === 'md' ? 'px-4 py-2' : 'px-6 py-3',
+        rounded: !props.rounded || props.type === 'link' || props.type === 'text' ? '' : `rounded-${props.size}`,
+        roundedFull: !props.roundedFull || props.type === 'link' || props.type === 'text' ? '' : 'rounded-full',
+        font: props.size === 'sm' ? 'text-sm leading-4.5' : 'text-base leading-6',
+        block: !props.block || props.type === 'link' ? '' : 'w-full',
+      }
+      return Object.values(styles).join(' ').toString().replace(/\s+/g, ' ')
     })
-    // const classTest = computed(() => {
-    //   return {
-    //     sizes: sizes(props.size),
-    //     types: types(props.type, props.variant),
-    //     font: font(props.size),
-    //     block: block(props.block),
-    //     rounded: rounded(props.rounded, props.size),
-    //     roundedFull: rounded(props.roundedFull),
-    //   }
-    // })
-    // console.log(filter, borderColor, textColor, bgColor, states, variant)
     return { classes }
   },
 }
