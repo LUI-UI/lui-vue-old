972f0628b83a4513a2cc4fcdb76edb586a7853fc
rhmkstk <rahmi.kstk@gmail.com>
2021-11-04 16:38:26 +0300

    create icon component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import LButton from './components/Button/LButton.vue'
+// import LIcon from './components/Icon/LIcons.vue'
 // import LBadge from './components/Badge/LBadge.vue'
 // import { reactive } from 'vue'
 </script>
@@ -10,12 +11,22 @@ import LButton from './components/Button/LButton.vue'
   <div class="btn-container">
     <l-button
       type="default"
-      variant="primary"
-      size="lg"
+      variant="secondary"
+      size="md"
+      block
       rounded-full
     >
       LTEST
     </l-button>
+    <!-- <hr>
+    <div class="color: red;">
+      <LIcon
+        name="home"
+        fill
+        class="text-red"
+      />
+    </div> -->
+    <!-- <i class="ri-admin-fill" /> -->
   </div>
 </template>
 
@@ -30,6 +41,7 @@ import LButton from './components/Button/LButton.vue'
 }
 .btn-container {
   padding: 150px;
+  font-size: 16px;
 }
 
 .badge-container {
---
+++ src/components/Icon/LIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <i
+    v-bind="$attrs"
+    :class="[
+      `ri-${name}${fill ? '-fill' : ''}${line ? '-line' : ''}`,
+      `ri-${size}`,
+    ]"
+  />
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: '1x',
+      validator(value) {
+        return [
+          'lg',
+          'xl',
+          'xxs',
+          'xs',
+          'sm',
+          'md',
+          '1x',
+          '2x',
+          '3x',
+          '4x',
+          '5x',
+          '6x',
+          '7x',
+          '8x',
+          '9x',
+          '10x',
+          'fw',
+        ].includes(value)
+      },
+    },
+    fill: {
+      type: Boolean,
+      default: false,
+    },
+    line: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
