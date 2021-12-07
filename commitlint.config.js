0fafdc39fdbe7b7799142cc9fba8ba35909681bf
rhmkstk <rahmi.kstk@gmail.com>
2021-12-07 15:14:46 +0300

    fix: add commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ['@commitlint/config-conventional']
 }
+
+// https://www.conventionalcommits.org/en/v1.0.0/#summary
+
+
+//build
+// chore
+// ci
+// docs
+// feat
+// fix
+// perf
+// refactor
+// revert
+// style
+// test
